17a146f4a295109aa227b4abc1060c3285d5580f
CJBiohacker <cj.moyses@gmail.com>
2022-02-19 03:15:22 -0300

    feat : Questão 2 resolvida e modifcada

--- Java/src/Questao2.java
+++ Java/src/Questao2.java
@@ -1,45 +1,45 @@
-import java.util.Scanner;
-import java.util.regex.Pattern;
-
 public class Questao2 {
 
-    // String[] upperMatch= { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-    //                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-    // String[] lowerMatch= {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    //                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-    // String[] specialChar = { "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+ "};
+    public boolean passwordMatch(String pwd ) {
+
+        int count = 0;
+        int pwdQty = ( 6 - pwd.length());
+
+        if ( pwd.length() >= 6 ) {
+            if ( pwd.matches(".*\\d.*") )
+                count ++;
+            if ( pwd.matches(".*[a-z].*") )
+                count ++;
+            if ( pwd.matches(".*[A-Z].*") )
+                count ++;
+            if ( pwd.matches(".*[!@#$%^&*()+-].*") )
+                count ++;
+        } else {
+            System.out.println("Sua senha precisa de mais " + pwdQty + " caracteres");
+        }
+
+        return count >= 3;
+    }
 
     public static void main(String[] args) {
 
-        String password = "Ya3";
-        String pwdCondition = "^(?=.*[0-9])" + "(?=.*[a-z])" + "(?=.*[A-Z])" + "(?=.*[!@#$%^&*()+-])" + "(?=\\S+$).{6,}$";
+        Questao2 pwdVal = new Questao2();
+        String password = "Ya3129038";
 
-        System.out.println("Crie uma senha conforme as condições abaixo:\n" +
-                "● Possui no mínimo 6 caracteres.\n" +
-                "● Contém no mínimo 1 digito.\n" +
-                "● Contém no mínimo 1 letra em minúsculo.\n" +
-                "● Contém no mínimo 1 letra em maiúsculo.\n" +
-                "● Contém no mínimo 1 caractere especial. Os caracteres especiais são: !@#$%^&*()-+ \n ");
+        System.out.println("""
+                Crie uma senha conforme as condições abaixo:
+                ● Possui no mínimo 6 caracteres.
+                ● Contém no mínimo 1 digito.
+                ● Contém no mínimo 1 letra em minúsculo.
+                ● Contém no mínimo 1 letra em maiúsculo.
+                ● Contém no mínimo 1 caractere especial. Os caracteres especiais são: !@#$%^&*()-+\s
+                \s""");
 
         System.out.println("Senha: " + password);
 
+        boolean result = pwdVal.passwordMatch(password);
 
-        boolean passwordMatch = Pattern.matches(pwdCondition,password );
-        int pwdQuantity = (6 - password.length());
+        System.out.println(result);
 
-        System.out.println(passwordMatch);
-
-        if ( password.length() < 6 && !passwordMatch) {
-            System.out.println("Sua senha precisa de mais " + pwdQuantity + " caracteres e não atende todos requisitos de caractere.");
-
-        } else if ( password.length() < 6 && passwordMatch){
-            System.out.println("Sua senha precisa de mais " + pwdQuantity + " caracteres, mesmo atendendo os requisitos de caratere.");
-
-        } else if ( password.length() > 6 && !passwordMatch) {
-            System.out.println("Sua senha não atende todos requisitos.");
-
-        } else {
-            System.out.println("Senha Válida.");
-        }
     }
 }
