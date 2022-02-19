472b5f9c824141a327c571b01cd32a37ef6318a5
CJBiohacker <cj.moyses@gmail.com>
2022-02-18 23:14:13 -0300

    feat : Questão 2 resolvida

--- Java/src/Questao2.java
+++ Java/src/Questao2.java
@@ -1,45 +1,45 @@
-import org.jetbrains.annotations.NotNull;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Questao2 {
 
-    // Possui no mínimo 6 caracteres. ==>
-    // Contém no mínimo 1 digito. ==>  [ 1,2,3,4,5,... ]
-    // Contém no mínimo 1 letra em minúsculo. ==> [ a, b, c, ... ]
-    // Contém no mínimo 1 letra em maiúsculo. ==> [ A, B, C, ... ]
-    // Contém no mínimo 1 caractere especial. Os caracteres especiais são: [ !, @, #, $, %, ^, &, *, (, ), -, + ]
-
-    public void compareUpperCase(String @NotNull [] array, String senha) {
-
-        boolean compare;
-
-        for ( String element: array) {
-            compare = senha == element;
-        }
-
-    }
-
+    // String[] upperMatch= { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+    //                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+    // String[] lowerMatch= {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    //                "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+    // String[] specialChar = { "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+ "};
 
     public static void main(String[] args) {
 
-        String senha = "Ya3";
-        int minimoSenha = 6;
+        String password = "Ya3";
+        String pwdCondition = "^(?=.*[0-9])" + "(?=.*[a-z])" + "(?=.*[A-Z])" + "(?=.*[!@#$%^&*()+-])" + "(?=\\S+$).{6,}$";
 
-        String[] upperCase= {"A",'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-        String[] lowerCase= {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-        String[] specialChar = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+' };
+        System.out.println("Crie uma senha conforme as condições abaixo:\n" +
+                "● Possui no mínimo 6 caracteres.\n" +
+                "● Contém no mínimo 1 digito.\n" +
+                "● Contém no mínimo 1 letra em minúsculo.\n" +
+                "● Contém no mínimo 1 letra em maiúsculo.\n" +
+                "● Contém no mínimo 1 caractere especial. Os caracteres especiais são: !@#$%^&*()-+ \n ");
 
-        Questao2 teste = new Questao2();
+        System.out.println("Senha: " + password);
 
-        teste.compareUpperCase();
 
-        if ( senha.length() <= minimoSenha ) {
-                if ( compare ) {
+        boolean passwordMatch = Pattern.matches(pwdCondition,password );
+        int pwdQuantity = (6 - password.length());
 
-                }
+        System.out.println(passwordMatch);
+
+        if ( password.length() < 6 && !passwordMatch) {
+            System.out.println("Sua senha precisa de mais " + pwdQuantity + " caracteres e não atende todos requisitos de caractere.");
+
+        } else if ( password.length() < 6 && passwordMatch){
+            System.out.println("Sua senha precisa de mais " + pwdQuantity + " caracteres, mesmo atendendo os requisitos de caratere.");
+
+        } else if ( password.length() > 6 && !passwordMatch) {
+            System.out.println("Sua senha não atende todos requisitos.");
+
+        } else {
+            System.out.println("Senha Válida.");
         }
-
-
     }
 }
